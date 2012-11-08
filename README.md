4a4b4d0668be93ce37dc1f6220f5d16fc8f92b30
James Ma <jamesma.1987@gmail.com>
2012-11-08 15:44:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Purdue FoodFinder
+=================
+
+Author: [James Ma][]
+
+Web application that lets you find events with free food around Purdue's campus. A work in progress.
+
+[James Ma]: http://jamesma.info
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Purduefoodfinder::Application.config.secret_token = '77bf0461b64479c20d209d3afecfb433534132ce3d52e1ece27256a046bd03647e71425251219da172fcdbfa28baf458d31e522bb6b1121de11ea6ac70f671d2'
