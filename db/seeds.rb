23d94b9a01a007b68ede014e14e669e82c3c175d
James Ma <jamesma.1987@gmail.com>
2012-11-09 00:59:36 -0500

    Update db/seeds.rb with valid data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 puts 'CREATING ROLES'
+
 Role.create([
   { :name => 'admin' }, 
   { :name => 'user' }, 
   { :name => 'VIP' }
 ], :without_protection => true)
+
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'foobar', :password_confirmation => 'foobar'
+
+admin = User.create! :name => 'Administrator', :email => 'ma23@purdue.edu', :password => 'foobar', :password_confirmation => 'foobar'
+admin.add_role :admin
+puts 'Admin created: ' << admin.name
+
+user = User.create! :name => 'First User', :email => 'user1@purdue.edu', :password => 'foobar', :password_confirmation => 'foobar'
+user.add_role :user
 puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'foobar', :password_confirmation => 'foobar'
+
+user2 = User.create! :name => 'Second User', :email => 'user2@purdue.edu', :password => 'foobar', :password_confirmation => 'foobar'
+user2.add_role :user
 puts 'New user created: ' << user2.name
-user.add_role :admin
-user2.add_role :VIP
